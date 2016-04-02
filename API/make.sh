c598eaa7da2e0f64bc367071c2e2047c0e6400f9
berthelol <loic.berthelot1@gmail.com>
2016-04-02 16:34:08 +0200

    ajout update

--- API/make.sh
+++ API/make.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-if [ $1 == 1 ]; then
-	g++ -o launch main.cpp -lcurl -std=c++11 -Wall 
-	./launch
-elif [ $1 == 2 ]; then
-	g++ -o launch RandomApp.cpp -lcurl -std=c++11 -Wall 
-	./launch
-else 
-	echo "No parameter or wrong parameter"
-fi
-
-#echo "Gcc completed"
+echo "Installing the last version of RED API code"
+git clone https://github.com/TeamBRAVA/RED-C-API.git
+echo "Cloning down"
+echo "Downloading latest compiler"
+sudo apt-get install g++
+echo "Downloading latest curl"
+sudo apt-get install libcurl4-openssl-dev
+echo "Done"
 
 
-#echo "DONE"
